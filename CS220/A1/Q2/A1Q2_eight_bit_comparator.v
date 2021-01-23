12a1702b053dd6ba2ea55db1a65b9d9ce4a24b0a
Sarthak Rout <sarthak2024@gmail.com>
2021-01-23 19:25:10 +0530

    Changed generate and display code

--- CS220/A1/Q2/A1Q2_eight_bit_comparator.v
+++ CS220/A1/Q2/A1Q2_eight_bit_comparator.v
@@ -15,15 +15,25 @@ module eight_bit_comparator(A, B, less, equal, greater);
 	wire [7:0] greater_8;
 
 	// One bit comparator for MSB (7th) position
-	one_bit_comparator cmsb(A[7], B[7], 1'b0, 1'b1, 1'b0, less_8[7], equal_8[7], greater_8[7]);
+	one_bit_comparator c7(A[7], B[7], 1'b0, 1'b1, 1'b0, less_8[7], equal_8[7], greater_8[7]);
+	one_bit_comparator c6(A[6], B[6], less_8[7], equal_8[7], greater_8[7], less_8[6], equal_8[6], greater_8[6]);
+	one_bit_comparator c5(A[5], B[5], less_8[6], equal_8[6], greater_8[6], less_8[5], equal_8[5], greater_8[5]);
+	one_bit_comparator c4(A[4], B[4], less_8[5], equal_8[5], greater_8[5], less_8[4], equal_8[4], greater_8[4]);
+	one_bit_comparator c3(A[3], B[3], less_8[4], equal_8[4], greater_8[4], less_8[3], equal_8[3], greater_8[3]);
+	one_bit_comparator c2(A[2], B[2], less_8[3], equal_8[3], greater_8[3], less_8[2], equal_8[2], greater_8[2]);
+	one_bit_comparator c1(A[1], B[1], less_8[2], equal_8[2], greater_8[2], less_8[1], equal_8[1], greater_8[1]);
+	one_bit_comparator c0(A[0], B[0], less_8[1], equal_8[1], greater_8[1], less_8[0], equal_8[0], greater_8[0]);
 
-	// Compile time loop for module instantiation of other 7 positions
+
+
+	/* Compile time loop for module instantiation of other 7 positions
 	genvar i;
 	generate
 		for (i = 0 ; i<7; i = i + 1) begin : bit_comp
 			one_bit_comparator c0(A[i], B[i], less_8[i+1], equal_8[i+1], greater_8[i+1], less_8[i], equal_8[i], greater_8[i]);
 		end 
 	endgenerate
+	*/
 	
 	
 	assign less = less_8[0];
--- CS220/A1/Q2/A1Q2_eight_bit_comparator_top.v
+++ CS220/A1/Q2/A1Q2_eight_bit_comparator_top.v
@@ -12,11 +12,6 @@ module eight_bit_comparator_top;
 	//Declare eight bit comparator module instantiation
 	eight_bit_comparator comp(A, B, less, equal, greater);
 
-	always @(A or B)
-		begin
-			$monitor("Time = %0t \tA = %b = %d, B = %b = %d, less = %b, equal = %b, greater = %b", $time, A, A, B, B, less, equal, greater);
-		end
-
 	//To specify finishing time
 	initial
 		begin
@@ -31,33 +26,44 @@ module eight_bit_comparator_top;
 			//default values: 
 			A = 0; B = 0;
 			#1
+			$display("Time = %0t \tA = %b = %d, B = %b = %d, less = %b, equal = %b, greater = %b", $time, A, A, B, B, less, equal, greater);
 			A = 8'b00001011; B = 8'b00000111; 
 			#1
+			$display("Time = %0t \tA = %b = %d, B = %b = %d, less = %b, equal = %b, greater = %b", $time, A, A, B, B, less, equal, greater);
 			A = 8'b10001011; B = 8'b00000111; 
 			#1
+			$display("Time = %0t \tA = %b = %d, B = %b = %d, less = %b, equal = %b, greater = %b", $time, A, A, B, B, less, equal, greater);
 			A = 8'b00001010; B = 8'b00001010;
 			#1
+			$display("Time = %0t \tA = %b = %d, B = %b = %d, less = %b, equal = %b, greater = %b", $time, A, A, B, B, less, equal, greater);
 			A = 8'b00001010; B = 8'b00000101;
 			#1
+			$display("Time = %0t \tA = %b = %d, B = %b = %d, less = %b, equal = %b, greater = %b", $time, A, A, B, B, less, equal, greater);
 			A = 8'b00000000; B = 8'b11111111;
 			#1
+			$display("Time = %0t \tA = %b = %d, B = %b = %d, less = %b, equal = %b, greater = %b", $time, A, A, B, B, less, equal, greater);
 			A = 8'b10101011; B = 8'b10101010;
 			#1 
+			$display("Time = %0t \tA = %b = %d, B = %b = %d, less = %b, equal = %b, greater = %b", $time, A, A, B, B, less, equal, greater);
 			A = 8'b10101010; B = 8'b10101011;
 			#1
+			$display("Time = %0t \tA = %b = %d, B = %b = %d, less = %b, equal = %b, greater = %b", $time, A, A, B, B, less, equal, greater);
 			A = 8'b10010101; B = 8'b11010000;
 			#1
+			$display("Time = %0t \tA = %b = %d, B = %b = %d, less = %b, equal = %b, greater = %b", $time, A, A, B, B, less, equal, greater);
 			A = 8'b10001011; B = 8'b10001011;
 			#1
-			A = 8'b10101111; B = 8'b01000100;
+			$display("Time = %0t \tA = %b = %d, B = %b = %d, less = %b, equal = %b, greater = %b", $time, A, A, B, B, less, equal, greater);
+			
 		end
+
 
 	/*
 	Can be used to visualise waveforms: 
 	initial begin
 		$dumpfile("comp.vcd");
 		$dumpvars(-1, eight_bit_comparator_top);
-		$monitor("Time = %0t \t, A = %b, B = %b, less = %b, equal = %b, greater = %b", $time, A, B, less, equal, greater);
+		$monitor("Time = %0t \t, A = %b, B = %b, less = %b, equal = %b, greater = %b , Test ID = %0d", $time, A, B, less, equal, greater, test_id);
 	end
 	*/
 endmodule
