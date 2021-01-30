0c4016b8085a63e7d7941c27824a6ae2256f16f3
Sarthak Rout <sarthak2024@gmail.com>
2021-01-30 10:30:49 +0530

    Q1 updated

--- CS220/A2/Q1/A2Q1_decoder3to8.v
+++ CS220/A2/Q1/A2Q1_decoder3to8.v
@@ -6,14 +6,14 @@ module decoder3to8(dec_in, dec_out);
 	output [7:0] dec_out;
 	wire [7:0] dec_out;
 
-	assign dec_out[0] = dec_in[0] & dec_in[1] & dec_in[2];
-	assign dec_out[1] = (~dec_in[0]) & dec_in[1] & dec_in[2];
-	assign dec_out[2] = dec_in[0] & (~dec_in[1]) & dec_in[2];
-	assign dec_out[3] = (~dec_in[0]) & (~dec_in[1]) & dec_in[2];
-	assign dec_out[4] = dec_in[0] & dec_in[1] & (~dec_in[2]);
-	assign dec_out[5] = (~dec_in[0]) & dec_in[1] & (~dec_in[2]);
-	assign dec_out[6] = dec_in[0] & (~dec_in[1]) & (~dec_in[2]);
-	assign dec_out[7] = (~dec_in[0]) & (~dec_in[1]) & (~dec_in[2]);
+	assign dec_out[0] = (~dec_in[2]) & (~dec_in[1]) & (~dec_in[0]); 	// 0 = 000
+	assign dec_out[1] = (~dec_in[2] & (~dec_in[1]) & dec_in[0] ); 		// 1 = 001
+	assign dec_out[2] = (~dec_in[2]) & dec_in[1] & (~dec_in[0]) ;		// 2 = 010
+	assign dec_out[3] = (~dec_in[2]) & dec_in[1] &  dec_in[0]; 			// 3 = 011
+	assign dec_out[4] = dec_in[2] & (~dec_in[1]) & (~dec_in[0]) ;		// 4 = 100
+	assign dec_out[5] = dec_in[2] & (~dec_in[1]) & dec_in[0] ;			// 5 = 101
+	assign dec_out[6] = dec_in[2] & dec_in[1] & (~dec_in[0]);			// 6 = 110
+	assign dec_out[7] = dec_in[2] & dec_in[1] & dec_in[0];				// 7 = 111
 
 	
 endmodule
--- CS220/A2/Q1/A2Q1_encoder8to3.v
+++ CS220/A2/Q1/A2Q1_encoder8to3.v
@@ -6,8 +6,8 @@ module encoder8to3(enc_in, enc_out);
 	output [2:0] enc_out;
 	wire [2:0] enc_out;
 
-	assign enc_out[0] = enc_in[0] | enc_in[2] | enc_in[4] | enc_in[6]; 
-	assign enc_out[1] = enc_in[0] | enc_in[1] | enc_in[4] | enc_in[5];
-	assign enc_out[2] = enc_in[0] | enc_in[1] | enc_in[2] | enc_in[3];
+	assign enc_out[0] = enc_in[1] | enc_in[3] | enc_in[5] | enc_in[7]; 		// 1, 3, 5, 7 have 1st bit as 1
+	assign enc_out[1] = enc_in[2] | enc_in[3] | enc_in[6] | enc_in[7];		// 2, 3, 6, 7 have 2nd bit as 1
+	assign enc_out[2] = enc_in[4] | enc_in[5] | enc_in[6] | enc_in[7];		// 4, 5, 6, 7 have 3rd bit as 1
 	
 endmodule
