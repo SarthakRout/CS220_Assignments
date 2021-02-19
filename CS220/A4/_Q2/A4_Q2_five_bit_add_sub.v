5f4410e8cfac93022acd6af7c59c3e3957247514
Devanshu Singla <devanshu803.singla@gmail.com>
2021-02-19 15:27:07 +0530

    add incomplete A4_Q2

---
+++ CS220/A4/_Q2/A4_Q2_five_bit_add_sub.v
@@ -0,0 +1,17 @@
+// module for eight bit adder/subtracter
+module five_bit_adder_subtracter (a, b, opcode, sum, carry, overflow);
+    input [4:0] a, b;   // operands
+    input opcode;       // operation code
+    output wire [4:0] sum;  // output of operation
+    output wire carry, overflow;    // carry over and overflow
+
+    wire [3:0] intermediate_carry;  // required dummy variable.
+
+    one_bit_adder_subtracter AB0 (a[0], b[0], opcode, opcode, sum[0], intermediate_carry[0]);
+    one_bit_adder_subtracter AB1 (a[1], b[1], intermediate_carry[0], opcode, sum[1], intermediate_carry[1]);
+    one_bit_adder_subtracter AB2 (a[2], b[2], intermediate_carry[1], opcode, sum[2], intermediate_carry[2]);
+    one_bit_adder_subtracter AB3 (a[3], b[3], intermediate_carry[2], opcode, sum[3], intermediate_carry[3]);
+    one_bit_adder_subtracter AB7 (a[4], b[5], intermediate_carry[3], opcode, sum[4], carry);
+
+    assign overflow = intermediate_carry[3]^carry;
+endmodule
---
+++ CS220/A4/_Q2/A4_Q2_worm.v
@@ -0,0 +1,61 @@
+// Module for Worm Game
+
+module worm(clk, dirn, step, x, y);
+	
+	input clk;
+	input [1:0] dirn;
+	input [1:0] step;
+
+	output reg[3:0] x;
+	output reg[3:0] y;
+
+	// Variables for ADD SUB Combinational Logic
+	wire [4:0] in_x;
+	wire [4:0] in_y;
+	wire [4:0] out_x;
+	wire [4:0] out_y;
+	wire [4:0] in_step;
+	wire over_x, over_y, carry_x, carry_y, op;
+	
+	assign in_x = {1'b0, x};
+	assign in_y = {1'b0, y};
+	assign in_step = {3'b000, step};
+	assign op = dirn[0];
+
+	five_bit_adder_subtracter X_OP(in_x, in_step, op, out_x, carry_x, over_x);
+	five_bit_adder_subtracter Y_OP(in_y, in_step, op, out_y, carry_y, over_y);
+
+	initial
+	begin
+		x = 4'b0000;
+		y = 4'b0000;
+	end
+
+	always @(out_x | over_x) begin
+		$display("Time: %3d, out_x: %b, over_x: %b", $time, out_x, over_x);
+	end
+
+	always @(posedge clk) begin
+		if (dirn[1] == 1'b0) begin
+			if (over_x == 1'b1)
+				x <= 4'b1111;
+			else begin
+				if (out_x[4] == 1'b1)
+					x <= 4'b0000;
+				else
+					x <= out_x[3:0];
+			end
+		end
+		else begin
+			if (over_y == 1'b1)
+				y <= 4'b1111;
+			else begin
+				if (out_y[4] == 1'b1)
+					y <= 4'b0000;
+				else
+					y <= out_y[3:0];
+			end
+		end
+	end
+
+endmodule
